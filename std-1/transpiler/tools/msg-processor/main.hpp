dbc96ff007c3414b378aadab060e227f5bbe8d89
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-21 19:00:53 +0200

    Create main.hpp

---
+++ std-1/transpiler/tools/msg-processor/main.hpp
@@ -0,0 +1,12 @@
+/**
+ * This file contains the definitions needed to initialise MSG-PROCESSOR tool!
+**/
+
+// in progress...
+
+// All elements related to the main initialisation of the MSG-PROCESSOR
+namespace msg_processor {
+
+    // The errors object!
+
+}
